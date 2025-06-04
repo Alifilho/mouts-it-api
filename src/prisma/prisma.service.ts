19c24ab1383f7b612278c6da5c272992df8795b0
Alisson Oliveira <alissonoliveiram@gmail.com>
2025-06-04 12:14:43 -0300

    feat: prisma module added

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({ log: ['query', 'info', 'warn', 'error'], errorFormat: 'pretty' });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
